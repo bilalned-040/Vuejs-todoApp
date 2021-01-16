d4ee83f20190352500f362d1998f20be37258ba4
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2021-01-16 12:35:39 +0500

    todo UI

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title">
+      <button type="submit">Add</button>
+    </form>
+  </div>
+</template>
+<script>
+
+import {v4} from 'uuid';
+const uuId = v4()
+export default {
+  name: 'AddTodo',
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodoObj = {
+        id: uuId,
+        title: this.title,
+        completed: false
+      }
+      this.$emit('add-todo', newTodoObj);
+      this.title = '';
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDQp72Xlk0G2gm0E3DkIAxvBUQ2Lhvifdg",
+    authDomain: "vuejs-todo-7c46d.firebaseapp.com",
+    projectId: "vuejs-todo-7c46d",
+    storageBucket: "vuejs-todo-7c46d.appspot.com",
+    messagingSenderId: "771566715674",
+    appId: "1:771566715674:web:2a8c201f549537faace2a3",
+    measurementId: "G-RF71EDX8EE"
+  }
+  firebase.initializeApp(firebaseConfig)
